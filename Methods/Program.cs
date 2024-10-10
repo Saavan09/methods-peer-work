331b99b5bc2faeaed6a36ff0d58b5df6d100fb4a
aceofcolours <saavantandon@gmail.com>
2024-10-10 14:47:23 -0400

    declared and initialized my (Saavan's) variables

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,6 +5,12 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
+        //Saavan's variable declarations
+        int saavanFavNumber;
+        int saavanLuckyNumber;
+
+        //Saavan's variable initializations
+        saavanFavNumber = 8;
+        saavanLuckyNumber = 9;
     }
 }
