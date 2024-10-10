69663be501d419e12303d4bef805703012672095
ShawnGHB <shawnshawn4116@gmail.com>
2024-10-10 15:08:27 -0400

    Added Subtract Function, Testing
    Added the subtraction function in side the main method and some test code

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -16,35 +16,43 @@ class Program
         int saavanLuckyNumber;
 
         //Shawn's variable declarations
-
-
+        int shawnFavNumber;
+        int shawnLuckyNumber;
 
         //Saavan's variable initializations
         saavanFavNumber = 8;
         saavanLuckyNumber = 9;
 
         //Shawn's variable initializations
+        shawnFavNumber = 43;
+        shawnLuckyNumber = 7;
 
-
+        int Subtract(int x, int y){
+            return (y - x);
+        }
 
         //start of program
         Console.WriteLine("Saavan's favorite number is " + saavanFavNumber + ", and his lucky number is " + saavanLuckyNumber + ".");
+        Console.WriteLine("Shawn's favorite number is " + shawnFavNumber + ", and his lucky number is " + shawnLuckyNumber + ".");
 
 
         //testing the functions
         int saavanAdd = Add(saavanFavNumber, saavanLuckyNumber);
+        int shawnSubtract = Subtract(shawnFavNumber, shawnLuckyNumber);
         Console.WriteLine("Adding Saavan's numbers..." + saavanFavNumber + " + " + saavanLuckyNumber + " = " + saavanAdd);
         //adding shawn's numbers
         //subtracting saavan's numbers
         //subtracting shawn's numbers
+        Console.WriteLine("Subtracting Shawn's numbers..." + shawnFavNumber + " - " + shawnLuckyNumber + " = " + shawnSubtract);
+
     }
 
-/// <summary>
-/// Takes in 2 integers and returns the result of them added together
-/// </summary>
-/// <param name="num1">First integer</param>
-/// <param name="num2">Second integer</param>
-/// <returns>Integer equal to num1 + num2</returns>
+    /// <summary>
+    /// Takes in 2 integers and returns the result of them added together
+    /// </summary>
+    /// <param name="num1">First integer</param>
+    /// <param name="num2">Second integer</param>
+    /// <returns>Integer equal to num1 + num2</returns>
     static int Add(int num1, int num2)
     {
         return num1 + num2;
